51497e4ccd331d41cc108cfa6a749572be836321
yousefalazmi9889-star <yousefalazmi9889@gmail.com>
2026-09-08 17:20:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from repl-nix-workspace!")
+import os
+import discord
+from discord.ext import commands
 
+intents = discord.Intents.default()
+intents.message_content = True
 
-if __name__ == "__main__":
-    main()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+bot.run(os.environ.get("DISCORD_TOKEN"))
